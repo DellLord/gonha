d4337d1f80c647e2167324c3262b21d141f9b87a
Fred Lins <fredcox@gmail.com>
2020-07-12 19:47:56 -0300

    I put QMainWindow visible in all workspaces. Its Ok.

--- gonha/__init__.py
+++ gonha/__init__.py
@@ -1,6 +1,9 @@
 import os
-from PyQt5 import QtWidgets, uic, QtCore
+import sys
+from PyQt5 import QtWidgets, uic, QtCore, QtGui
+from ewmh import EWMH
 
+app = QtWidgets.QApplication(sys.argv)
 resource_path = os.path.join(os.path.split(__file__)[0], './')
 
 
@@ -14,5 +17,27 @@ class MainWindow(QtWidgets.QMainWindow):
         self.hourLabel = self.findChild(QtWidgets.QLabel, 'hourLabel')
         self.setWindowFlags(flags)
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        # self.move(2400, 0)
+        self.topRight()
         self.show()
+        # Show in all workspaces
+        ew = EWMH()
+        all_wins = ew.getClientList()
+        wins = filter(lambda w: w.get_wm_class()[1] == 'gonha', all_wins)
+        for w in wins:
+            print(w)
+            ew.setWmDesktop(w, 0xffffffff)
 
+        ew.display.flush()
+
+    def topRight(self):
+        screen = app.primaryScreen()
+        print('Screen: %s' % screen.name())
+        size = screen.size()
+        print('Size: %d x %d' % (size.width(), size.height()))
+        rect = screen.availableGeometry()
+        print('Available: %d x %d' % (rect.width(), rect.height()))
+        # move window to top right
+        win = self.geometry()
+        print(f'mainwindow size {win.width()} x {win.height()}')
+        self.move(rect.width() - win.width(), 0)
